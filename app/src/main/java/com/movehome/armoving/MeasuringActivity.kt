143a79d12327ad65e7ecd7cafee3c28d4fabe54d
HoWanJu <58217957+HoWanJu@users.noreply.github.com>
2020-09-16 23:20:16 +0800

    merged tabView

--- app/src/main/java/com/movehome/armoving/MeasuringActivity.kt
+++ app/src/main/java/com/movehome/armoving/MeasuringActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 
-public class MeasuringActivity(intent: Intent) : AppCompatActivity() {
+class MeasuringActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
